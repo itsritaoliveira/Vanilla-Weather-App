67ae2f8f08261ee0ac1a3e40bd070ab24a4d8973
itsritaoliveira <160160643+itsritaoliveira@users.noreply.github.com>
2024-05-08 22:59:50 +0100

    Created basic javascript (no API)

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function searchCity(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city");
+  let input = document.querySelector("#search-form");
+  cityElement.innerHTML = input.value;
+}
 
+let form = document.querySelector("#form");
+form.addEventListener("submit", searchCity);
