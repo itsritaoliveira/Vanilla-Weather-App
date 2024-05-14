a4922d3294a496bed0f5621e6cdfe009e8cc90a7
itsritaoliveira <160160643+itsritaoliveira@users.noreply.github.com>
2024-05-14 19:15:01 +0100

    Added weather API

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function searchCity(event) {
-  event.preventDefault();
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
   let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "b6df588aced7eba41d5oe20f4t632103";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function searchSubmit(event) {
+  event.preventDefault();
   let input = document.querySelector("#search-form");
-  cityElement.innerHTML = input.value;
+  searchCity(input.value);
 }
 
 let form = document.querySelector("#form");
-form.addEventListener("submit", searchCity);
+form.addEventListener("submit", searchSubmit);
+
+searchCity("Lisbon");
