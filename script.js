e2d30e075317c930abc4c56d1aca032d24c5a1ec
itsritaoliveira <160160643+itsritaoliveira@users.noreply.github.com>
2024-05-28 11:42:46 +0100

    Updated time data and temperature values

--- script.js
+++ script.js
@@ -3,7 +3,38 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   temperatureElement.innerHTML = Math.round(temperature);
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity-value");
+  let windElement = document.querySelector("#wind-value");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = `${formatDate(date)},`;
+
+  console.log(response.data);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
